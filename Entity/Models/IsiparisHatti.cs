09ebf4266e3aed167e963e80a2d04fd4ddf3f484
batuhan.fındık <batuhan.fndk@BK-L102-PC02>
2021-07-27 14:11:06 +0300

    .

---
+++ Entity/Models/IsiparisHatti.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity.Models
+{
+    public interface IsiparisHatti
+    {
+        int Id { get; set; }
+        string siparisHattiIcon { get; set; }
+        string siparisHattiYazi { get; set; }
+        int siparisHattiTel { get; set; }
+    }
+}
